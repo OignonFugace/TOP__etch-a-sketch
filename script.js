e0ce39a748d665e7fc3aa9cfe70ae54a3dd3114e
Tanguy <tanguy.freycon@gmail.com>
2022-12-07 16:17:13 +0100

    Add grid sizing functionnality and basic drawing functionnality

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 
 const container = document.getElementById('container');
+const gridSizeBtn = document.getElementById('gridSizeBtn');
+let isDrawing = false;
 
-let numberOfDiv = prompt('Side size of the grid? ');
+gridSizeBtn.addEventListener('click', changeGridSize);
 
-container.style.setProperty('grid-template-columns', `repeat(${numberOfDiv}, 1fr)`);
+let numberOfDiv = 16;
 
-for (let i = 0; i < numberOfDiv ** 2; i++) {
-    const div = document.createElement('div');
-    container.appendChild(div);
+makeGrid(null, numberOfDiv);
+
+function changeGridSize(e) {
+    numberOfDiv = prompt('Side size of the grid ? ');
+    makeGrid(e, numberOfDiv);
+}
+
+function makeGrid(e, numberOfDiv) {
+    container.style.setProperty('grid-template-columns', `repeat(${numberOfDiv}, 1fr)`);
+    Array.from(container.children).forEach(element => {
+        element.remove();
+    });
+    for (let i = 0; i < numberOfDiv ** 2; i++) {
+        const div = document.createElement('div');
+        // div.addEventListener('mouseover', function(e) { colorDiv(e, 'black') });
+        // div.addEventListener('mouseenter', function(e) { colorDiv(e, '#999') })
+        // div.addEventListener('mouseleave', function(e) { uncolorDiv(e) });
+        div.addEventListener('mousedown', e => isDrawing = true);
+        div.addEventListener('mouseup', e => isDrawing = false);
+        div.addEventListener('mousemove', e => {
+            if (isDrawing) draw(e, 'black');
+        });
+        container.addEventListener('mouseleave', e => isDrawing = false);
+        container.appendChild(div);
+    }
+}
+
+function colorDiv(e, color) {
+    e.target.style.backgroundColor = color;
+}
+
+function uncolorDiv(e) {
+    e.target.style.backgroundColor = '';
+}
+
+function draw(context) {
+    colorDiv(context, 'black');
 }
