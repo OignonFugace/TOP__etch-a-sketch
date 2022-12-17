af9b1995672eb53f6146bb5322c077810718c092
Tanguy <tanguy.freycon@gmail.com>
2022-12-17 22:43:51 +0100

    Create closure scope

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
+'use strict';
 
-const container = document.getElementById('container');
-const gridSizeBtn = document.getElementById('gridSizeBtn');
-let isDrawing = false;
+function makeApp() {
 
-gridSizeBtn.addEventListener('click', changeGridSize);
+    /* VARIABLES DECLARATION */
+    const container = document.getElementById('container');
+    const gridSizeBtn = document.getElementById('gridSizeBtn');
+    let isDrawing = false;
+    let numberOfDiv = 16;
 
-let numberOfDiv = 16;
-
-makeGrid(null, numberOfDiv);
-
-function changeGridSize(e) {
-    numberOfDiv = prompt('Side size of the grid ? ');
-    makeGrid(e, numberOfDiv);
-}
-
-function makeGrid(e, numberOfDiv) {
-    container.style.setProperty('grid-template-columns', `repeat(${numberOfDiv}, 1fr)`);
-    Array.from(container.children).forEach(element => {
-        element.remove();
-    });
-    for (let i = 0; i < numberOfDiv ** 2; i++) {
-        const div = document.createElement('div');
-        // div.addEventListener('mouseover', function(e) { colorDiv(e, 'black') });
-        // div.addEventListener('mouseenter', function(e) { colorDiv(e, '#999') })
-        // div.addEventListener('mouseleave', function(e) { uncolorDiv(e) });
-        div.addEventListener('mousedown', e => isDrawing = true);
-        div.addEventListener('mouseup', e => isDrawing = false);
-        div.addEventListener('mousemove', e => {
-            if (isDrawing) draw(e, 'black');
-        });
-        container.addEventListener('mouseleave', e => isDrawing = false);
-        container.appendChild(div);
+    function makeGrid(e, numberOfDiv) {
+        container.style.setProperty('grid-template-columns', `repeat(${numberOfDiv}, 1fr)`);
+        Array.from(container.children).forEach(element => element.remove());
+        for (let i = 0; i < numberOfDiv ** 2; i++) {
+            const div = document.createElement('div');
+            div.addEventListener('mousedown', e => isDrawing = true);
+            div.addEventListener('mouseup', e => isDrawing = false);
+            div.addEventListener('mousemove', e => {
+                if (isDrawing === true) draw(e, 'black');
+            });
+            container.appendChild(div);
+        }
     }
+
+    function draw(context) {
+        colorDiv(context, 'black');
+    }
+
+    function colorDiv(e, color) {
+        e.target.style.backgroundColor = color;
+    }
+
+    function uncolorDiv(e) {
+        e.target.style.backgroundColor = '';
+    }
+
+    function changeGridSize(e) {
+        numberOfDiv = prompt('Side size of the grid ? ');
+        makeGrid(e, numberOfDiv);
+    }
+
+    function app() {
+        window.addEventListener('mouseup', e => isDrawing = false);
+        window.addEventListener('mouseleave', e => isDrawing = false);
+        gridSizeBtn.addEventListener('click', changeGridSize);
+        makeGrid(null, numberOfDiv);
+    }
+
+    return app;
 }
 
-function colorDiv(e, color) {
-    e.target.style.backgroundColor = color;
-}
-
-function uncolorDiv(e) {
-    e.target.style.backgroundColor = '';
-}
-
-function draw(context) {
-    colorDiv(context, 'black');
-}
+let app = makeApp();
+app();
